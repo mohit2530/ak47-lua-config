b6a9d1cf97e187520646d6c688390212ebf56b1d
Mohit <44540219+mohit2530@users.noreply.github.com>
2026-05-14 07:49:42 -0600

    Create init.lua
    Created main lua file for minimalistic nvim configuration.

---
+++ init.lua
@@ -0,0 +1,137 @@
+-- =========================
+-- BASIC SETTINGS
+-- =========================
+
+vim.g.mapleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.scrolloff = 8
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+
+
+-- =========================
+-- LAZY.NVIM BOOTSTRAP
+-- =========================
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+
+-- =========================
+-- PLUGINS
+-- =========================
+
+require("lazy").setup({
+
+  -- Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require("telescope.builtin")
+
+      vim.keymap.set("n", "<leader>ff", builtin.find_files)
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+      vim.keymap.set("n", "<leader>fb", builtin.buffers)
+      vim.keymap.set("n", "<leader>fr", builtin.oldfiles)
+    end,
+  },
+
+  -- Mason
+  {
+    "williamboman/mason.nvim",
+    config = true,
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "ts_ls",
+        },
+      })
+    end,
+  },
+
+  -- LSP (NEW API ONLY)
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+
+      -- enable servers
+      vim.lsp.enable({
+        "ts_ls",
+      })
+
+      -- ts_ls config (NEW STYLE)
+      vim.lsp.config("ts_ls", {
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+        settings = {
+          typescript = {
+            suggest = { autoImports = true },
+            updateImportsOnFileMove = { enabled = true },
+          },
+          javascript = {
+            suggest = { autoImports = true },
+            updateImportsOnFileMove = { enabled = true },
+          },
+        },
+      })
+
+      -- keymaps
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+      vim.keymap.set("n", "K", vim.lsp.buf.hover)
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+    end,
+  },
+
+  -- Autocomplete
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+      })
+    end,
+  },
+
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+
+})
